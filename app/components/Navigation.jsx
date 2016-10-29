1b2f4ba01699b83beeb4f016e232a2fa85099aad
Vitalii Nalyvaiko <v.yu.nalyvaiko@gmail.com>
2016-10-29 15:48:03 +0300

    Initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,29 @@
+const React = require('react');
+const {Link, IndexLink} = require('react-router');
+
+const Navigation = () => {
+    return (
+        <div className="top-bar">
+            <div className="top-bar-left">
+                <ul className="menu">
+                    <li className="menu-text">Timer Application</li>
+                    <li>
+                        <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+                    </li>
+                    <li>
+                        <Link to="/" activeClassName="active-link">Countdown</Link>
+                    </li>
+                </ul>
+            </div>
+            <div className="top-bar-right">
+                <ul className="menu">
+                    <li className="menu-text">
+                        Created by <a href="https://github.com/Nalyvaiko" target="_blank">Vitalii Nalyvaiko</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+module.exports = Navigation;
